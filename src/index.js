a7a7b6df24023f425ce318dfc32cdf7c6b9bbe35
qmarr <komarenetsv@gmail.com>
2021-03-13 17:09:41 +0200

    feat: add solution for brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let opening = '', closing = '';
+  let finalArray = str.split('');
+  for(let i = 0; i < bracketsConfig.length; i++) {
+    opening = bracketsConfig[i][0];
+    closing = bracketsConfig[i][1];
+  
+  
+    for(let j = 0; j < finalArray.length; j++) {
+      if(finalArray[j] == opening && finalArray[ j + 1 ] == closing) {
+        finalArray.splice(j, 2);
+        j = -1;
+        i = -1
+      }
+    }
+  }
+  if(finalArray.length == 0)
+    return true;
+  return false
 }
